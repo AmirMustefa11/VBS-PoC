dc6833d8481d3299a7ae35ad1a8fccd556dd8a63
AmirMustefa11 <amirmustefa707@gmail.com>
2022-10-18 16:49:04 +0300

    minor changes

--- src/pages/components/index.js
+++ src/pages/components/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Image from 'next/image'
-import styles from './index.module.css'
 
 import { marketing, applicationUi, ecommerce } from '@/constants/navbarElements'
 
